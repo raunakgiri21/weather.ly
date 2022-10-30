7699123b2068328c5e4b65c8c5eea793f0c24382
Raunak <raunakgiri21@gmail.com>
2022-10-30 17:49:08 +0530

    Forecast and place suggestions

--- app.js
+++ app.js
@@ -13,19 +13,47 @@ const _pressure = document.querySelector('.pressure');
 const _humidity = document.querySelector('.humidity');
 const _windspeed = document.querySelector('.windspeed');
 const _visibility = document.querySelector('.visibility');
-const _sunrise = document.querySelector('.sunrise');
-const _sunset = document.querySelector('.sunset');
 
+const _searchBar = document.querySelector('.searchBar');
+const _suggestions = document.querySelector('.suggestions');
+const _options = document.querySelectorAll('option');
 const _weatherIcon = document.querySelector('.weather-icon');
 const _switch = document.querySelector('#checkbox');
-const _iconImg = document.querySelector('.weather-icon')
 const _card = document.querySelector('.card');
 const _underline = document.querySelector('.title-underline');
 
+const _hourlyCards = document.querySelector('.hourly-cards');
+const _dailyCards = document.querySelector('.daily-cards');
+
+_searchBar.addEventListener('input', function(e) {
+    const target = e.target.value
+    if(target.length > 1){
+        fetch( `https://api.openweathermap.org/geo/1.0/direct?q=${target}&limit=4&appid=${weatherApiKey}`).then(res =>res.json()).then(data => suggest(data))
+    }
+    else{
+        _suggestions.innerHTML = '';
+    }
+})
+
+const suggest = (data) => {
+    _suggestions.innerHTML = "";
+    for(let x of data){
+        _suggestions.innerHTML += `<option data-lat=${x.lat} data-lon=${x.lon} data-name=${x.name}>${x.name}, ${x.state}</option>`;
+    }
+}
+
+_suggestions.addEventListener('click',function(e){
+    let curTarget= e.target.attributes;
+    // console.log(curTarget['data-lat'].value)
+    lat = curTarget['data-lat'].value;
+    long = curTarget['data-lon'].value;
+    _suggestions.innerHTML = "";
+    fettch().then(() => {_searchBar.value = "";})
+})
+
 _switch.addEventListener('change',function(){
     _card.classList.toggle('night-card');
-    _iconImg.classList.toggle('night-img');
-    _underline.classList.toggle('night-title-underline');
+    _searchBar.classList.toggle('night-bar');
 });
 
 const backgroundList = function(id,icon){
@@ -76,14 +104,13 @@ const updateHTML= function (){
     _humidity.textContent =`humidity: ${humidity} %`;
     _windspeed.textContent = `windspeed: ${windspeed} kmph`;
     _visibility.textContent = `visibility: ${visibility} km`;
-    _sunrise.textContent = `sunrise: ${sunrise}`;
-    _sunset.textContent = `sunset: ${sunset}`;
     _weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon">`;
     _container.style.background = `url('./images/${backgroundList(id,icon)}.jpg') center/cover no-repeat fixed`
 }
 
-const fettch = function(){
-    data = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${weatherApiKey}`).then(res=> res.json()).then(data=> {
+const fettch = async function(){
+    let currHourlyForecast = '';
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${weatherApiKey}`).then(res=> res.json()).then(data=> {
         place = data.name;
         temperature = Math.trunc(data.main.temp - 273.15);
         description = data.weather[0].description;
@@ -93,17 +120,81 @@ const fettch = function(){
         visibility = (data.visibility/1000).toFixed(1);
         id = data.weather[0].id;
         icon = data.weather[0].icon;
-        fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=${timezoneApi}&format=json&by=position&lat=${lat}&lng=${long}`).then(res=> res.json()).then(data2 => {
-            sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-US',{timeZone:data2.zoneName, hour: 'numeric', minute: 'numeric', hour12: true});
-            sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString('en-US',{timeZone:data2.zoneName, hour: 'numeric', minute: 'numeric', hour12: true});
-            updateHTML()
-        }).catch(err => {
-            console.log('timezonedb error: ',err)
-            sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-US',{timeZone:'Asia/Kolkata', hour: 'numeric', minute: 'numeric', hour12: true});
-            sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString('en-US',{timeZone:'Asia/Kolkata', hour: 'numeric', minute: 'numeric', hour12: true});
-            updateHTML()
-        })
-    });
+        updateHTML();
+        currHourlyForecast = `<div class="single-card">
+                <p class="time">Now</p>
+                <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="card icon" class="wicon">
+                <p class="card-temp">${temperature.toFixed(0)}°C</p>
+            </div>`
+    })
+    await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${weatherApiKey}&units=metric`).then(res => res.json()).then(data => {
+        const dayName = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        let fiveDays = {'0':{max: -99, min: 99, icon:'09d'},'1':{max: -99, min: 99, icon:'09d'},'2':{max: -99, min: 99, icon:'09d'},'3':{max: -99, min: 99, icon:'09d'},'4':{max: -99, min: 99, icon:'09d'},'5':{max: -99, min: 99, icon:'09d'},'6':{max: -99, min: 99, icon:'09d'}};
+        const forecast = data.list.map((item) => {
+            const {temp, temp_max, temp_min} = item.main;
+            const {dt, dt_txt} = item;
+            const [{description, icon}] = item.weather ;
+            const c = new Date(dt_txt);
+            fiveDays[`${c.getDay()}`].max = (fiveDays[`${c.getDay()}`].max < temp_max) ? temp_max : fiveDays[`${c.getDay()}`].max;
+            fiveDays[`${c.getDay()}`].min = (fiveDays[`${c.getDay()}`].min > temp_min) ? temp_min : fiveDays[`${c.getDay()}`].min;
+            fiveDays[`${c.getDay()}`].icon = icon;
+            return ( { temp, temp_max, temp_min, dt, dt_txt, description, icon });
+        });
+        const todayForcast = forecast.slice(2,10);
+
+        const hourlyHtml = todayForcast.map(item => {
+            let date1 = new Date(item.dt_txt)
+            const temp = item.temp;
+            let icon = item.icon;
+            date1 = date1.getHours() ;
+            const ampm = (date1 > 11) ? 'PM' : 'AM';
+            if(date1 < 18 && date1 > 6){
+                icon = icon.split('');
+                icon[2] = 'd';
+                icon = icon.join('');
+            }
+            return (
+                `<div class="single-card">
+                <p class="time">${date1 % 12 || 12} ${ampm}</p>
+                <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="card icon" class="wicon">
+                <p class="card-temp">${temp.toFixed(0)}°C</p>
+              </div>`
+            )
+        });
+        _hourlyCards.innerHTML = currHourlyForecast + hourlyHtml.join('');
+
+
+        let dailyHtml = 8;
+        let count= new Date();
+        count = count.getDay();
+        while(fiveDays[`${count}`].max === 99){
+            count++;
+        }
+        // console.log(fiveDays[`${count}`])
+        dailyHtml = `<div class="single-card-daily">
+                        <p class="day">Today</p>
+                        <img src="http://openweathermap.org/img/wn/${fiveDays[`${count}`].icon}@2x.png" alt="card icon" class="wicon">
+                        <p class="card-temp-max">${(fiveDays[`${count}`].max).toFixed(0)}°C</p>
+                        <p class="card-temp-min">${(fiveDays[`${count}`].min).toFixed(0)}°C</p>
+                    </div>`;
+        const stop = count;
+        count++;  
+        // console.log(count)          
+        while(count !== stop){
+            if(fiveDays[`${count}`].max !== -99)
+            {
+                dailyHtml += `<div class="single-card-daily">
+                <p class="day">${dayName[count]}</p>
+                <img src="http://openweathermap.org/img/wn/${fiveDays[`${count}`].icon}@2x.png" alt="card icon" class="wicon">
+                <p class="card-temp-max">${(fiveDays[`${count}`].max).toFixed(0)}°C</p>
+                <p class="card-temp-min">${(fiveDays[`${count}`].min).toFixed(0)}°C</p>
+                </div>`;
+            }         
+            count = (count+1)%7;
+        }
+        _dailyCards.innerHTML = dailyHtml;
+
+    })
 }
 
 const smallScreen = function(){
@@ -113,8 +204,6 @@ const smallScreen = function(){
         <p class="humidity">Humidity: ${humidity}%</p>
         <p class="windspeed">windspeed: ${windspeed} kmph</p>
         <p class="visibility">visibility: ${visibility} km</p>
-        <p class="sunrise">sunrise: ${sunrise}</p>
-        <p class="sunset">sunset: ${sunset}</p>
       </div>`;
     }
 }
